82368c3cf3d2a801c7734ad6f7f8be74fd0e23b7
오하마 <59410853+HAMA-coder@users.noreply.github.com>
2020-01-13 20:59:47 +0900

    python study day 6
    # 정수 
    
    이번 파트에서는 파이썬에서 사칙연산이 어떤 기호로 이루어지는지 복습하고자 한다.
    +, -, /, *, //, %, ** 기호는 각각 더하기, 빼기, 나눗셈, 곱하기, 몫, 나머지, 제곱을 의미한다.
    c언어 과목에서 배우지 않은 거듭제곱 기호가 특별했다.
    
    c언어에서는 5를 2로 나누어 실수로 표시할 때
    5 또는 2 변수 선언시 한 쪽을 실수 float으로 선언해야한다.
    그러나 파이썬에서는 그러한 수고로움 없이 사칙연산 기호로 실수표기가 가능하다.
    
    # 관련 함수
    divmod(5,2)에서 divmod는 좌측의 수를 우측의 수로 나눈 값의 몫과 나머지를 출력하는 기능을 한다.
    이때 출력되는 두 가지 수는 변수를 선언하여 각각의 변수에 저장될 수 있음을 알았다.
    
    # 실수
    1. print(4.2+5)
    9.2가 출력되었다. 즉, 실수와 정수의 합을 출력하고자 할때 파이썬은 더 넓은 범주의 타입으로 출력함을 알 수 있었다.
    
    2. print(5); print(float(5))
    각각 5와 5.0이 출력되었다. 5는 정수이지만 이때 float을 입혀서 출력할 경우 실수로 표기되어 출력될 수 있음을 알았다.
    
    # 복소수
    -1에 root를 씌운 값을 수학에서는 기호 'i'로 표시하지만 파이썬에서는 기호 'j'로 표시한다.
    이때 함수 complex(a,b) //여기서 a와 b는 실수 값이다.
    'a'는 실수 부분, 'b'는  허수 부분의 수에 해당된다.

---
+++ python_base_day 6.py
@@ -0,0 +1,32 @@
+# int operation
+print(1+1)
+
+print(1-3)
+
+print(2*1)
+
+print(4**2)
+
+print(5/2)
+
+print(5//2)
+
+print(5%2)
+
+# function
+print(divmod(5,2))
+
+A,B=divmod(5,2)
+
+print((A,B))
+
+# real number
+print(4.2+5)
+
+print(5)
+print(float(5))
+
+# complex number
+print(1.3+1.3j)
+print(complex(1.3, 1.3))
+
