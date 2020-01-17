d77f5376a2cd5684d7a5978bd63e8ccabe3be832
오하마 <59410853+HAMA-coder@users.noreply.github.com>
2020-01-17 19:49:41 +0900

    python study day 8
    # 줄바꿈
     print(1,2,3)을 입력하면 1 2 3으로 사이 빈공간을 두고 출력된다. 문득 궁금해졌다. 문자 사이 공간을 더할 수 없을까?
    sep='(값 사이에 넣고 싶은 값)'을 이용하면 이것이 가능해짐을 배웠다.
    즉 print(1,2,3,sep=' and ')라고 할 경우 1 and 2 and 3이 출력된다.
    내가 원하는대로 숫자 사이 여백을 더하고 싶으면 print(1,2,3,sep=' (원하는 여백의 크기) ')를 입력하면 된다.
    print(1,2,3,sep'\n')의 경우는 줄바꿈이, print(1,2,3,sep='\t')는 tap키를 눌렀을 때 발생하는 여백이 입력된 형식으로 출력되게 한다.
    
    # 논리연산자
     and 기호는 한쪽이라도 거짓이면 거짓으로, or 기호는 한쪽이라도 참이면 참으로 출력하였다.
    이때 'not True and False or not False'는 어떤 결과일지 궁금해졌다. 결과는 True였다.
    왼쪽부터 읽으면 False and False으로 False가 되고 최종적으로 False or True 이므로 True가 출력된 것이다.
    
    # '=='과 'is'의 차이
    파이썬에서 쓰이는 연산자 'is'가 '=='과 어떤 차이가 있는지 궁금해서 1.0과 1을 비교해보았다.
    print(1==1.0); print(1 is 1.0)의 결과 전자는 True, 후자는 False가 출력되었다. 왜 그런 것일까?
    'is'는 객체를 비교하는 연산자이기 때문이다. 1은 정수, 1.0은 실수이기 때문이라고도 할 수 있다. 
    사실 확인을 위해 메모리 주소를 알아내는 함수 id(1.0)과 id(1)을 사용해보니 메모리 값이 서로 다르게 나왔다.
     
    # 줄바꿈(문장)
    python 3.8(32-bit)프로그램을 실행하여 문장을 세 단위로 띄어쓰려했다.
    print("hello");print("this is");print("Python")으로 해결할 수 있었으나 중복되는 print문이 마음에 들지 않았다.
    hello='''hi
    this is
    Python.'''
    print(hello)를 이용하면 print문의 중복된 사용 없이 세 줄 띄어쓰기가 가능함을 배웠다.
    반대로 hello를 지정할 필요없이 
    print('''hello
    this is
    Python.''')도 같은 결과로 출력이 가능함을 알 수 있었다.

---
+++ python_base_day 8.py
@@ -0,0 +1,22 @@
+# 줄바꿈
+print(1,2,3,sep=' ')
+
+print(1,2,3,sep='\n')
+
+print(1,2,3,sep='\t')
+
+# 논리연산자
+print(True and False)
+print(True or False)
+print(not True and False or not False)
+
+# '=='과 'is'의 차이
+print(1==1.0)
+print(1 is 1.0)
+
+# 줄바꿈
+hello='''hello world! 
+this is
+Python.'''
+
+print(hello)
