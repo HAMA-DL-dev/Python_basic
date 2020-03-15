a021165c5a04306670a9985850751fbd46839a6b
런닝머신뛰는 하연이 <59410853+HAMA-coder@users.noreply.github.com>
2020-03-15 17:14:47 +0900

    자료형의 종류
    """설명하기에 앞서, 자료형이 무엇인가 의문이 들 것입니다.
    모든 프로그래밍 언어는 자료형의 개념을 가지고 있는데, 
    사람이 문자, 문자열, 정수, 실수가 무엇인지 전달하기 위해 컴퓨터의 언어인
    프로그래밍 언어 또한 그에 상응한 개념을 내포합니다. 쉽게 예시를 들어보죠
    
    ex)
    1 → 정수 자료형 int(integer의 약자)에 해당됨
    2.8 → 실수 자료형 float에 해당됨
    'a' → 문자 자료형에 해당됨 ,str(string)
    'hello' → 문자열 자료형에 해당됨, str(string)
    
    아래에서 언급한 자료형은 Python에서 등장하는, 조금 새로울지도 모르는 자료형입니다.
    list, tuple, dictionary, bool, set 중에서 이번에는 list가 무엇인지 알아보려합니다.
    """

--- data_type.py
+++ data_type.py
@@ -1,17 +1,23 @@
+
 #1 type : list
 days=["mon","tue","wed","thur","fri"]
 print(type(days)) 
+
+
 
 #2 function
 days=["mon","tue","wed","thur","fri"]
 days.append("sat")
 print(days)
 
+
+
 #3 multiple function
 days=["mon","tue","wed","thur","fri"]
 days.append("sat")
 days.reverse()
 print(days)
+
 
 
 #learned what is 'list'
